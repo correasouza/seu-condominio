e1892dc021bf5669a39eb55bfcd495285afcbeb8
Igor Correa <igorsantosigor272@gmail.com>
2025-08-09 11:05:33 -0300

    fix: adicionando validações ao crud de tarefas

--- backend/app/models/tarefa.rb
+++ backend/app/models/tarefa.rb
@@ -2,4 +2,9 @@ class Tarefa < ApplicationRecord
     enum :status, { concluido: 0, pendente: 1, andamento: 2 }
 
     validates :nome, :data_inicio, :data_conclusao, :custo, :status, presence: true
+
+    validates :data_conclusao, comparison: { greater_than_or_equal_to: :data_inicio }
+
+    validates :nome, length: { in: 3..60, message: "O nome precisa ter entre 3 e 60 caracteres." }
+
 end
